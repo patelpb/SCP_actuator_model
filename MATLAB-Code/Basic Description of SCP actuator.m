2a1fbe576c45b3768a515c4f636e9cb7918a48b0
patelpb <patelpb@ucla.edu>
2016-11-27 21:07:59 -0800

    Update and rename Basic Description of SCP actuator to Basic Description of SCP actuator.m

---
+++ MATLAB-Code/Basic Description of SCP actuator.m
@@ -0,0 +1,17 @@
+
+% c =  thermal constant
+% Tamb = ambient temperature
+% b = damping coefficient
+% v = velocity 
+% x0 = equilibrium position 
+% F = mg
+T = (F - bv - k(x-x0) + cTamb)/c;
+
+%L = absolute thermal conductivity of SCP in ambient environment
+%T = current temperature
+%cth = thermal mass of SCP actuator
+
+P = Cth ((T- Tamb) / (t_final - t_initial)) + L(T-Tamb);
+
+%R = resistance of SCP actuator
+V = sqrt (P*R);
